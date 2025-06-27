e5bd84873e28d2edc79bb48b74a48c1b9dc6db36
Alexandra Eliseeva <saridormi123@gmail.com>
2025-06-27 11:36:21 +0200

    clean up

--- graphs/envbench_graphs/readonly/graph.py
+++ graphs/envbench_graphs/readonly/graph.py
@@ -43,15 +43,7 @@ def create_read_only_workflow(
         }
 
     async def call_tools(state: EnvSetupReadOnlyState, config: RunnableConfig) -> EnvSetupReadOnlyState:
-        # pass tools_kwargs from state to tool node
-        tools_kwargs = state.get("tools_kwargs", [{}])
-        print("[DEBUG] tools_kwargs:", tools_kwargs)
-        tools_kwargs = tools_kwargs[0]
-        if "configurable" in config:
-            config["configurable"]["tools_kwargs"] = tools_kwargs
-        else:
-            config["configurable"] = {"tools_kwargs": tools_kwargs}
-        response = await tool_node.ainvoke(state["messages"], config=config)
+        response = await tool_node.ainvoke(state["messages"])
         return {"messages": response, "turn": state["turn"] + 1}
 
     async def force_submit_shell_script_tool_call(
